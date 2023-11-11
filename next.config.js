5ad624885c8b5fb5e070e5f0b13b77b2e19c4fb8
Michiel Koning <mail@michielkoning.nl>
2023-11-11 19:55:27 +0100

    pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -11,4 +15,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
